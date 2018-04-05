42f8b4532af2782ea2f636ff4c9cd96d3e2baf2f
Cedric Meury <cedric@meury.com>
2018-04-05 20:47:50 +0200

    correcting shellcheck warnings

--- helpers/ktx-preview.sh
+++ helpers/ktx-preview.sh
@@ -5,12 +5,13 @@ set -eu
 
 selected=$1
 
-IFS=';' read -ra context <<< "$(kubectl config view -o jsonpath="{range .contexts[?(@.name == '${selected}')].context}{@.cluster};{@.user};{@.namespace};{end}")"
-cluster=${context[0]}
-user=${context[1]}
-namespace=${context[2]}
+context=$(kubectl config view -o jsonpath="{range .contexts[?(@.name == '${selected}')].context}{@.cluster};{@.user};{@.namespace}{end}")
+cluster=$(echo "${context}" | cut -d ';' -f 1)
+user=$(echo "${context}" | cut -d ';' -f 2)
+namespace=$(echo "${context}" | cut -d ';' -f 3)
 
 server=$(kubectl config view -o jsonpath="{.clusters[?(@.name == '${cluster}')].cluster.server}")
 
-echo "=== ${selected} ===\n"
-echo "Cluster ${cluster};Server ${server};User ${user};Namespace ${namespace};" | tr ';' "\n" | column -t
+echo "=== ${selected} ==="
+echo
+echo "Cluster ${cluster};Server ${server};User ${user};Namespace ${namespace};" | tr ';' '\n' | column -t
